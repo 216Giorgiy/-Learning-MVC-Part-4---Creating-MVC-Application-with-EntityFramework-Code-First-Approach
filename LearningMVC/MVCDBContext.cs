64d549f8a736db3a8c84e9e3d1f392e99c3d659a
Akhil Mittal <akhil.mittal20@gmail.com>
2016-04-07 14:51:10 +0530

    Source Code
    Source Code

---
+++ LearningMVC/MVCDBContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using LearningMVC.Models;
+
+namespace LearningMVC
+{
+    public class MVCDBContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
